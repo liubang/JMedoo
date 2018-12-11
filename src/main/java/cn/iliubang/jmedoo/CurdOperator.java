84af89c18abfa1e0cc03bdda3ca8f3c0ceb06c25
liubang <it.liubang@gmail.com>
2018-12-11 21:25:31 +0800

    use optional for select operation

--- src/main/java/cn/iliubang/jmedoo/CurdOperator.java
+++ src/main/java/cn/iliubang/jmedoo/CurdOperator.java
@@ -7,6 +7,7 @@ import cn.iliubang.jmedoo.sharding.ShardingStrategyInterface;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.GenericTypeResolver;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -14,10 +15,7 @@ import org.springframework.jdbc.support.KeyHolder;
 
 import java.sql.PreparedStatement;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author <a href="mailto:it.liubang.gmail.com">liubang</a>
@@ -56,7 +54,8 @@ public abstract class CurdOperator<T> {
             List<String> list = new ArrayList<>();
             if (keys.length > 0) {
                 if (null == map || map.isEmpty()) {
-                    throw new RuntimeException("Map should contain all keys of " + Arrays.toString(keys) + ", but null or empty given.");
+                    throw new RuntimeException("Map should contain all keys of " + Arrays.toString(keys)
+                            + ", but null or empty given.");
                 }
                 for (String key : keys) {
                     if (map.containsKey(key)) {
@@ -80,12 +79,14 @@ public abstract class CurdOperator<T> {
 
     public long add(T type, Map<String, String> tMap) {
         KeyHolder keyHolder = new GeneratedKeyHolder();
-        SqlBuilder.SqlObjects sqlObjects = new SqlBuilder().buildInsert(getTableName(entryClass, tMap), entryClass, type);
+        SqlBuilder.SqlObjects sqlObjects = new SqlBuilder().buildInsert(getTableName(entryClass, tMap),
+                entryClass, type);
 
         logger.info(sqlObjects.toString());
 
         int res = jdbcTemplateMaster.update(con -> {
-            PreparedStatement preparedStatement = con.prepareStatement(sqlObjects.getSql(), Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement preparedStatement = con.prepareStatement(sqlObjects.getSql(),
+                    Statement.RETURN_GENERATED_KEYS);
             int index = 0;
             for (Object o : sqlObjects.getObjects()) {
                 index++;
@@ -105,40 +106,75 @@ public abstract class CurdOperator<T> {
         return add(type, null);
     }
 
-    public List<T> select(Query query, Map<String, String> tMap) throws SqlParseException {
+    public Optional<List<T>> select(Query query, Map<String, String> tMap) throws SqlParseException {
         SqlBuilder.SqlObjects sqlObjects = new SqlBuilder().buildSelect(getTableName(entryClass, tMap), query);
         logger.info(sqlObjects.toString());
-        return jdbcTemplateSlave.query(sqlObjects.getSql(), sqlObjects.getObjects(), new BeanPropertyRowMapper<>(entryClass));
+        List<T> tList = jdbcTemplateSlave.query(sqlObjects.getSql(), sqlObjects.getObjects(),
+                new BeanPropertyRowMapper<>(entryClass));
+        if (null == tList || tList.isEmpty()) {
+            return Optional.empty();
+        } else {
+            return Optional.of(tList);
+        }
     }
 
-    public List<T> select(Query query) throws SqlParseException {
+    public Optional<List<T>> select(Query query) throws SqlParseException {
         SqlBuilder.SqlObjects sqlObjects = new SqlBuilder().buildSelect(getTableName(entryClass), query);
         logger.info(sqlObjects.toString());
-        return jdbcTemplateSlave.query(sqlObjects.getSql(), sqlObjects.getObjects(), new BeanPropertyRowMapper<>(entryClass));
+        List<T> tList = jdbcTemplateSlave.query(sqlObjects.getSql(), sqlObjects.getObjects(),
+                new BeanPropertyRowMapper<>(entryClass));
+        if (null == tList || tList.isEmpty()) {
+            return Optional.empty();
+        } else {
+            return Optional.of(tList);
+        }
     }
 
-    public T get(Query query, Map<String, String> tMap) throws SqlParseException {
+    public Optional<T> get(Query query, Map<String, String> tMap) throws SqlParseException {
         SqlBuilder.SqlObjects sqlObjects = new SqlBuilder().buildSelect(getTableName(entryClass, tMap), query);
         logger.info(sqlObjects.toString());
-        return jdbcTemplateSlave.queryForObject(sqlObjects.getSql(), sqlObjects.getObjects(), new BeanPropertyRowMapper<>(entryClass));
+        try {
+            T t = jdbcTemplateSlave.queryForObject(sqlObjects.getSql(), sqlObjects.getObjects(),
+                    new BeanPropertyRowMapper<>(entryClass));
+            return Optional.of(t);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+
     }
 
-    public T get(Query query) throws SqlParseException {
+    public Optional<T> get(Query query) throws SqlParseException {
         SqlBuilder.SqlObjects sqlObjects = new SqlBuilder().buildSelect(getTableName(entryClass), query);
         logger.info(sqlObjects.toString());
-        return jdbcTemplateSlave.queryForObject(sqlObjects.getSql(), sqlObjects.getObjects(), new BeanPropertyRowMapper<>(entryClass));
+        try {
+            T t = jdbcTemplateSlave.queryForObject(sqlObjects.getSql(), sqlObjects.getObjects(),
+                    new BeanPropertyRowMapper<>(entryClass));
+            return Optional.of(t);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     public Long count(Query query, Map<String, String> tMap) throws SqlParseException {
         SqlBuilder.SqlObjects sqlObjects = new SqlBuilder().buildCount(getTableName(entryClass, tMap), query);
         logger.info(sqlObjects.toString());
-        return jdbcTemplateSlave.queryForObject(sqlObjects.getSql(), sqlObjects.getObjects(), Long.class);
+        Long co = jdbcTemplateSlave.queryForObject(sqlObjects.getSql(), sqlObjects.getObjects(), Long.class);
+        if (null == co) {
+            return 0L;
+        } else {
+            return co;
+        }
     }
 
     public Long count(Query query) throws SqlParseException {
         SqlBuilder.SqlObjects sqlObjects = new SqlBuilder().buildCount(getTableName(entryClass), query);
         logger.info(sqlObjects.toString());
-        return jdbcTemplateSlave.queryForObject(sqlObjects.getSql(), sqlObjects.getObjects(), Long.class);
+        Long co = jdbcTemplateSlave.queryForObject(sqlObjects.getSql(), sqlObjects.getObjects(), Long.class);
+        if (null == co) {
+            return 0L;
+        } else {
+            return co;
+        }
     }
 
     public int update(T type) {
@@ -148,7 +184,8 @@ public abstract class CurdOperator<T> {
     }
 
     public int update(T type, Map<String, String> tMap) {
-        SqlBuilder.SqlObjects sqlObjects = new SqlBuilder().buildUpdate(getTableName(entryClass, tMap), entryClass, type);
+        SqlBuilder.SqlObjects sqlObjects = new SqlBuilder().buildUpdate(getTableName(entryClass, tMap),
+                entryClass, type);
         logger.info(sqlObjects.toString());
         return jdbcTemplateMaster.update(sqlObjects.getSql(), sqlObjects.getObjects());
     }
